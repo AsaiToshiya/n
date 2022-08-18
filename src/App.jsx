b7caca54289d26ae5e73a0efe2f7fa108630396f
Asai Toshiya <to.asai.60@gmail.com>
2022-08-19 00:00:54 +0900

    trim しないようにする
    Closes #35

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
   const handleListClick = () => setListShow(!isListShow);
 
   const handleNewClick = () => {
-    notes[0].text.trim() === ""
+    notes[0].text === ""
       ? setSelectedKeys([notes[0].id])
       : setSelectedKeys([addNote().id]);
   };
@@ -72,7 +72,7 @@ function App() {
   };
 
   const listItems = notes.map((note) => ({
-    label: note.text.trim() ? note.text : " ",
+    label: note.text ? note.text : " ",
     key: note.id,
   }));
 
